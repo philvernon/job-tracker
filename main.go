623b7d4c80950fa8d2eabc1fdc6db17e07836791
phil <philip.s.vernon@gmail.com>
2026-02-18 12:39:21 +0000

    add cors middleware

--- main.go
+++ main.go
@@ -11,6 +11,21 @@ import (
 	"github.com/philvernon/job-tracker/internal/repository"
 )
 
+func withCORS(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Access-Control-Allow-Origin", "http://localhost:5174")
+		w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS")
+		w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+
+		if r.Method == http.MethodOptions {
+			w.WriteHeader(http.StatusOK)
+			return
+		}
+
+		next.ServeHTTP(w, r)
+	})
+}
+
 func main() {
 	db, err := db.Open()
 	if err != nil {
@@ -64,12 +79,14 @@ func main() {
 		}
 
 		w.Header().Set("Content-Type", "application/json")
-		w.WriteHeader(http.StatusCreated)
+		w.WriteHeader(http.StatusOK)
 		json.NewEncoder(w).Encode(allJobs)
 	})
 
+	handler := withCORS(mux)
+
 	log.Println("Server running on :8080")
-	err = http.ListenAndServe(":8080", mux)
+	err = http.ListenAndServe(":8080", handler)
 	if err != nil {
 		log.Fatal(err)
 	}
